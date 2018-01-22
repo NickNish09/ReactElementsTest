d1701214488afd5fa8b79bd216657c8f9ce82397
Nicholas <nicholasmarques@cjr.org.br>
2018-01-22 03:06:12 -0200

    adicionada navegacao bottom ao app

--- App.js
+++ App.js
@@ -1,56 +1,38 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Container, Header, Title, View, Icon, DeckSwiper, Card, CardItem, Thumbnail, Content, Footer, FooterTab, Button, Left, Right, Body, Text } from 'native-base';
-import Contacts from './src/Contacts';
-
+import { Tabs } from './src/Router.js';
+import Contacts from './src/Contacts.js';
 export default class App extends Component {
-
 
     render() {
         return (
             <Container>
-            <Header style={{backgroundColor: 'purple'}}
-                    androidStatusBarColor='#b769c6'
-            >
-                <Left style={{flex: 1}}>
-                    <Button transparent
-                    >
-                        <Icon name='menu'
-                              color='#ffffff'
-                        />
-                    </Button>
-                </Left>
-                <Body style={{alignItems: 'center',flex: 1}}>
-                <Title >First App</Title>
-                </Body>
-                <Right style={{flex: 1}}>
-                    <Button transparent>
-                        <Icon name='home'
-                              color='#ffffff'
-                        />
-                    </Button>
-                </Right>
-            </Header>
-            <Content contentContainerStyle={{padding: 5}}>
-                <Contacts/>
-            </Content>
-                <Footer>
-                    <FooterTab style={{backgroundColor: 'purple'}}>
-                    <Button>
-                        <Icon name="apps"/>
-                            </Button>
-                        <Button>
-                            <Icon name="camera" />
+                <Header style={{backgroundColor: 'purple'}}
+                        androidStatusBarColor='#b769c6'
+                >
+                    <Left style={{flex: 1}}>
+                        <Button transparent
+                        >
+                            <Icon name='menu'
+                                  color='#ffffff'
+                            />
                         </Button>
-                        <Button>
-                        <Icon active name="navigate" />
-                            </Button>
-                        <Button active>
-                            <Icon name="person" />
+                    </Left>
+                    <Body style={{alignItems: 'center',flex: 1}}>
+                    <Title >First App</Title>
+                    </Body>
+                    <Right style={{flex: 1}}>
+                        <Button transparent>
+                            <Icon name='home'
+                                  color='#ffffff'
+                            />
                         </Button>
-                        </FooterTab>
-                </Footer>
+                    </Right>
+                </Header>
+                <Tabs />
             </Container>
+
         );
     }
 }
--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,38 @@
 import React from 'react';
 import { TabNavigator } from 'react-navigation';
+import { Icon } from 'native-base';
 
 import Contacts from './Contacts.js';
+import Home from './Home.js';
+
+export const Tabs = TabNavigator({
+    Contacts: {
+        screen: Contacts,
+        navigationOptions: {
+            tabBarLabel: 'Contatos',
+            tabBarIcon: ({ tintColor }) => <Icon name="person" size={26} style={{ color: tintColor }} />,
+        }
+    },
+    Home: {
+        screen: Home,
+        navigationOptions: {
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ tintColor }) => <Icon name="home" size={26} style={{ color: tintColor }} />,
+        }
+    },
+},  {
+    headerMode: 'none',        // I don't want a NavBar at top
+    tabBarPosition: 'bottom',  // So your Android tabs go bottom
+    tabBarOptions: {
+        activeTintColor: '#fff',  // Color of tab when pressed
+        inactiveTintColor: '#fff', // Color of tab when not pressed
+        showIcon: 'true', // Shows an icon for both iOS and Android
+        showLabel: false,
+        labelStyle: {
+            fontSize: 11,
+        },
+        style: {
+            backgroundColor: 'purple', // Makes Android tab bar white instead of standard blue
+        }
+    },
+});
